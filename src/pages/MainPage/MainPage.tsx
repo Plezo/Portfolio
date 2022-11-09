0b0709a168df58cdcf39aa8b22ddaef7b80abe12
Plezo <Plezo@protonmail.com>
2022-11-08 20:04:46 -0500

    Styling

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -41,7 +41,7 @@ function MainPage() {
             I'm a software engineer who develops full-stack applications. I'm always working on projects
             to expand my skillset as well as working with people who help me grow as a software engineer.
             The entire process fascinates me from the planning stages of a project to laying the finishing touches.
-            And most importantly, I'm addicted to the process of learning new things.
+            And most importantly, I'm in love with the process of learning new things.
           </p>
         </div>
 
