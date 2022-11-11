a7d669414378694ef520abe46af7cbd27e06f904
Plezo <Plezo@protonmail.com>
2022-11-11 18:01:17 -0500

    Remade frontend

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,58 +1,106 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-import Project from '../../components/Main/Project'
 import Footer from '../../components/Main/Footer'
 import Panel from '../../components/Panel/Panel'
 
 import { BsMoonFill } from 'react-icons/bs'
 
-import projects from './ProjectData.js'
-import './MainPage.css'
-
 function MainPage() {
 
-  const [darkMode, setDarkMode] = useState(true);
+  const [mode, setMode] = useState('d');
   const [showContactPanel, setShowContactPanel] = useState(false);
 
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark' || theme === 'light')
+      setMode(theme)
+  }, [])
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(mode === 'dark' ? 'light' : 'dark')
+    root.classList.add(mode)
+
+    localStorage.setItem('theme', mode);
+  }, [mode])
+
   return (
-    <>
+    <div className={`h-screen bg-ivory dark:bg-dark-purple`}
+    >
       { 
-        showContactPanel ? <Panel setShowContactPanel={setShowContactPanel}/> : null
+        // showContactPanel ? <Panel setShowContactPanel={setShowContactPanel}/> : null
       }
 
-      <header>
-        <button id='modeButton'>
+      <header className='flex pt-6 px-20'>
+        <button 
+        className={`${mode} 
+        bg-royal-purple w-10 h-10 rounded-2xl text-ivory text-xl
+        flex items-center align-center justify-center
+        mr-auto hover:bg-opacity-80 active:bg-dark-purple
+        dark:bg-lavender dark:text-dark-purple dark:active:bg-royal-purple
+        `}
+        onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+        >
           <BsMoonFill />
         </button>
-        <button id='meetingButton' onClick={() => setShowContactPanel(true)}>
+        <button 
+        className={`${mode} 
+        text-royal-purple font-semibold ml-auto text-2xl
+        hover:text-opacity-80 focus:text-dark-purple
+        dark:text-lavender dark:focus:text-royal-purple
+        `}
+        onClick={() => setShowContactPanel(true)}>
           Contact
         </button>
       </header>
 
-      <div id='main'>
-        <div id='intro'>
-          <h1>
+      <div 
+      className='flex flex-col align-middle mx-36 mt-28'
+      >
+        <div
+        className='w-8/12'
+        >
+          <h1
+          className={`${mode} 
+          flex-auto text-8xl font-bold text-royal-purple 
+          dark:text-lavender
+          `}
+          >
             Patryk
           </h1>
-          <h2>
+          <h2
+          className={`${mode} 
+          flex-auto text-3xl font-medium text-dark-purple mt-12
+          dark:text-ivory
+          `}
+          >
             Software Engineer & Full Stack Developer
           </h2>
-          <p>
+          <p
+          className={`${mode} 
+          flex-auto w-7/12 text-dark-purple leading-6 mt-6
+          dark:text-ivory
+          `}
+          >
             I'm a software engineer who develops full-stack applications. I'm always working on projects
             to expand my skillset as well as working with people who help me grow as a software engineer.
             The entire process fascinates me from the planning stages of a project to laying the finishing touches.
             And most importantly, I'm in love with the process of learning new things.
           </p>
+          <button
+          className={`${mode} 
+          bg-royal-purple text-ivory h-12 w-32 mt-8 rounded-3xl transition-all
+          hover:bg-opacity-80 active:bg-dark-purple
+          dark:bg-lavender dark:text-dark-purple
+          dark:active:bg-royal-purple
+          `}
+          >
+            Resume/CV
+          </button>
         </div>
-
-        <div id='projectList'>
-          {projects.map((obj: any): JSX.Element => {
-            return <Project info={obj}/>
-          })}
-        </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
-    </>
+    </div>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
@@ -8,29 +9,21 @@ module.exports = {
     './public/index.html',
     './public/**/*.tsx'
   ],
-  // darkMode: 'class',
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        purple: {
-          50: '#fdf8f6',
-          100: '#f2e8e5',
-          200: '#eaddd7',
-          300: '#e0cec7',
-          400: '#d2bab0',
-          500: '#bfa094',
-          600: '#a18072',
-          700: '#977669',
-          800: '#846358',
-          900: '#43302b',
-        },
-      }
+        'ivory': '#FFFEF2',
+        'dark-purple': '#2E1A47',
+        'royal-purple': '#663399',
+        'lavender': '#BB91E5',
+      },
     },
   },
   plugins: [],
   variants: {
     extend: {
-        backgroundColor: ['active'],
+        backgroundColor: ['active']
     },
 },
 }
